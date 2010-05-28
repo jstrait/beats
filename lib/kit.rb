4f6dcd8d3c5326c5719e533cc0735d69fecbc76b
jstrait <joelstrait@hotmail.com>
2010-05-28 10:29:36 -0400

    Documentation improvements for Kit.rb. In particular, adding class description.

--- lib/kit.rb
+++ lib/kit.rb
@@ -1,25 +1,52 @@
+# Raised when trying to load a sound file which can't be found at the path specified
 class SoundNotFoundError < RuntimeError; end
 
+# This class keeps track of the sounds that are used in a song. It provides a
+# central place for storing sound data, and most usefully, handles converting
+# sounds in different formats to a standard format.
+# 
+# For example, if a song requires a sound that is mono/8-bit, another that is
+# stereo/8-bit, and another that is stereo/16-bit, it can't mix them together
+# because they are in different formats. Kit however automatically handles the
+# details of converting them to a common format. If you add sounds to the kit
+# using add(), sounds that you get using get_sample_data() will be in a canonical
+# format.
+#
+# When choosing the canonical format, Kit uses the highest quality of the available
+# sounds. So if a mono/8-bit, stereo/8-bit, and stereo/16-bit sound are added,
+# when you retrieve each one using get_sample_data() they will be stereo/16-bit.
+#
+# Note that this means that each time a new sound is added to the Kit, the canonical
+# format might change, if the incoming sound has a higher bits per sample or
+# number of channels than any of the existing sounds. Therefore, all of the sounds
+# used by a Song should be added to the Kit before generation begins. If you
+# create Song objects by using SongParser, this will be taken care of for you (as
+# long as you don't modify the Kit afterward).
 class Kit
   PATH_SEPARATOR = File.const_get("SEPARATOR")
   
+  # Creates a new Kit object. base_path indicates the folder from which sound files
+  # with relative file paths will be loaded from.
   def initialize(base_path)
     @base_path = base_path
     @label_mappings = {}
     @sounds = {}
-    @num_channels = 0
-    @bits_per_sample = 0
+    @num_channels = 0     # Set to 0 so the first sound added will ratchet it to a real value
+    @bits_per_sample = 0  # Set to 0 so the first sound added will ratchet it to a real value
   end
   
+  # Adds a new sound to the kit. 
   def add(name, path)
     if(!@sounds.has_key? name)
-      if(!path.start_with?(PATH_SEPARATOR))
+      path_is_relative = !path.start_with?(PATH_SEPARATOR)
+      if(path_is_relative)
         full_path = @base_path + PATH_SEPARATOR + path
       end
       
       begin
         wavefile = WaveFile.open(full_path)
       rescue
+        # TODO: Raise different error if sound is in an unsupported format
         raise SoundNotFoundError, "Sound file #{full_path} not found."
       end
       
@@ -37,7 +64,7 @@ class Kit
     end
   end
   
-  # Returns the sample data for a sound contained in the Kit.
+  # Returns the sample data (as an Array) for a sound contained in the Kit.
   # Raises an error if the sound doesn't exist in the Kit.
   def get_sample_data(name)
     wavefile = @sounds[name]
@@ -52,6 +79,7 @@ class Kit
     end
   end
   
+  # Returns the number of sounds currently contained in the kit.
   def size
     return @sounds.length
   end
