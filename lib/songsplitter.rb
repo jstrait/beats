027cd642b8738586d30591bea9d1ce4ce8630f78
jstrait <joelstrait@hotmail.com>
2010-05-28 16:12:40 -1000

    Adding SongSplitter class.
    Takes a Song, and spawns a new Song for each Track. This way, to generate split output, you just use the normal combined generation code for each track-song. This removes the need to have separate logic in the core engine for generating combined and split output. This commit should allow the generation code to be simplified in future commits.
    
    bin/beats has been updated to use this new class for generating split output. It is also using SongOptimizer on split output, so split generation is now faster than before. (The old code is being kept temporarily for profiling purposes).

---
+++ lib/songsplitter.rb
@@ -0,0 +1,39 @@
+class SongSplitter
+  def initialize()
+  end
+  
+  def split(original_song)
+    track_names = original_song.track_names()
+    
+    split_songs = []
+    track_names.each do |track_name|
+      new_song = original_song.copy_ignoring_patterns_and_structure()
+      
+      if(track_name == "placeholder")
+        track_sample_data = []
+      else
+        track_sample_data = new_song.kit.get_sample_data(track_name)
+      end
+      
+      original_song.patterns.each do |name, original_pattern|
+        new_pattern = new_song.pattern name
+        
+        if(original_pattern.tracks.keys.member?(track_name))
+          new_pattern.track track_name,
+                            track_sample_data,
+                            original_pattern.tracks[track_name].rhythm
+        else
+          new_pattern.track track_name,
+                            track_sample_data,
+                            "." * original_pattern.tick_count()
+        end
+      end
+      
+      new_song.structure = original_song.structure
+      
+      split_songs << new_song
+    end
+    
+    return split_songs
+  end
+end
