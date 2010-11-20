201d1b95862ce294f747604095dbe355f25541f0
jstrait <joelstrait@hotmail.com>
2010-11-20 18:21:46 -0500

    Minor cleanup of AudioEngine tests.

--- test/audioengine_test.rb
+++ test/audioengine_test.rb
@@ -8,26 +8,25 @@ class AudioEngineTest < Test::Unit::TestCase
               :example_no_kit,
               :example_with_kit]
 
-  def load_song_fixtures
-    test_songs = {}
+  def load_fixtures
+    test_engines = {}
     base_path = File.dirname(__FILE__) + "/.."
     song_parser = SongParser.new()
     
-    test_songs[:blank] = AudioEngine.new(Song.new(), Kit.new(base_path, {}))
-
+    test_engines[:blank] = AudioEngine.new(Song.new(), Kit.new(base_path, {}))
 
     FIXTURES.each do |fixture_name|
       song, kit = song_parser.parse(base_path, YAML.load_file("test/fixtures/valid/#{fixture_name}.txt"))
-      test_songs[fixture_name] = AudioEngine.new(song, kit)
+      test_engines[fixture_name] = AudioEngine.new(song, kit)
     end
      
-    return test_songs 
+    return test_engines 
   end
 
   def test_initialize
-    test_songs = load_song_fixtures()
+    test_engines = load_fixtures()
 
-    assert_equal(5512.5, test_songs[:blank].tick_sample_length)
-    assert_equal(6615.0, test_songs[:repeats_not_specified].tick_sample_length)
+    assert_equal(5512.5, test_engines[:blank].tick_sample_length)
+    assert_equal(6615.0, test_engines[:repeats_not_specified].tick_sample_length)
   end
 end
