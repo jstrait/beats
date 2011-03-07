7b18185fc828d2cc906eb4d7dd578c7772253d93
jstrait <joelstrait@hotmail.com>
2011-03-06 22:10:20 -0500

    Grab bag improvements to website

--- web/examples.php
+++ web/examples.php
@@ -8,19 +8,19 @@
     <h2>Synth Groove</h2>
     <p>Drum kit: <a href="/download#drumkit-hammerhead">Hammerhead</a></p>
     <label class="example-song">Raw BEATS</label>
-    <audio class="example-song" controls="controls">
+    <audio class="example-song" controls>
       <source src="/media/beats_drum_machine_synth_groove_raw.mp3" />
       <source src="/media/beats_drum_machine_synth_groove_raw.ogg" />
       Your browser can't play this audio file.
     </audio>
     <label class="example-song">Full Song</label>
-    <audio class="example-song" controls="controls">
+    <audio class="example-song" controls>
       <source src="/media/beats_drum_machine_synth_groove.mp3" />
       <source src="/media/beats_drum_machine_synth_groove.ogg" />
       Your browser can't play this audio file.
     </audio>
     <div>
-      <a id="toggle-synth-groove" href="#" onclick="toggleSource('toggle-synth-groove', 'source-synth-groove'); return false;">Show Source</a>
+      <a id="toggle-synth-groove" class="toggle" href="#" onclick="toggleSource('toggle-synth-groove', 'source-synth-groove'); return false;">Show Source</a>
       <pre id="source-synth-groove" style="display: none;"><code>Song:
   Tempo: 105
   Flow:
@@ -32,7 +32,7 @@
     - deep_bass:   house_1_1.wav
     - clap:        compurhythm_8000_3.wav
     - hihat:       compurhythm_78_4.wav
-    - hihat2:      human_beatbox_4.wav
+    - shaker:      human_beatbox_4.wav
     - tom:         coron_drum_synce_ds7_2.wav
     - bongo_high:  bongo_3.wav
     - bongo_low:   bongo_2.wav
@@ -42,7 +42,7 @@ Verse:
   - deep_bass:   |.....X.X........|
   - clap:        |....X.......X...|
   - hihat:       |X.X..X.XX.X.X.X.|
-  - hihat2:      |............XXXX|
+  - shaker:      |............XXXX|
   - tom:         |X...............|
 
 Chorus:
@@ -62,7 +62,9 @@ function toggleSource(toggleId, elementId) {
   var element = $(elementId);  
 
   element.toggle();
-  toggler.innerText = (element.visible()) ? "Hide Source" : "Show source"; 
+  toggler.innerText = (element.visible()) ? "Hide Source" : "Show source";
+
+  return false;
 }
 </script>
 <? drawFooter(); ?>
