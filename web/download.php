b2063edbbf5dfae8534003be05cec2b569139606
jstrait <joelstrait@hotmail.com>
2011-06-04 16:22:22 -0400

    Updating installation instructions on website

--- web/download.php
+++ web/download.php
@@ -3,10 +3,11 @@
   <div class="content-box">
     <h2>Installing BEATS</h2>
     <p>Run the following from the command line:</p>
-    <p><code><pre class="single-line">sudo gem install beats</pre></code></p>
+    <p><code><pre class="single-line">gem install beats</pre></code></p>
     <p>This will download BEATS from <a href="http://rubygems.org/gems/beats">rubygems.org</a> and add a <code>beats</code> command to your path.</p>
+    <p>(Note that if you are installing using the default version of Ruby that comes with MacOS X, you might get a file permission error. If that happens, try <code>sudo gem install beats</code> instead. If you are using RVM, plain <code>gem install beats</code> should work fine.)</p>
     <p>The current version of BEATS is 1.2.1. If you have an older version, you can update with this:</p>
-    <p><code><pre class="single-line">sudo gem update beats</pre></code></p>
+    <p><code><pre class="single-line">gem update beats</pre></code></p>
     <p>Not sure what version you have? Run <code>beats -v</code>.</p>
   </div>
   <div class="content-box" id="drum-kits">
