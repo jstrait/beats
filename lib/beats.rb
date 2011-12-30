78973222ca9247a283785762908d6adc7ff5b1b4
jstrait <joelstrait@hotmail.com>
2011-12-30 13:48:08 -0500

    Bumping version to 1.2.3alpha

--- lib/beats.rb
+++ lib/beats.rb
@@ -1,5 +1,5 @@
 class Beats
-  BEATS_VERSION = "1.2.2"
+  BEATS_VERSION = "1.2.3alpha"
  
   # Each pattern in the song will be split up into sub patterns that have at most this many steps.
   # In general, audio for several shorter patterns can be generated more quickly than for one long
