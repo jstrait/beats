04d69dd117bdb864ec61e6a44b1ef85007577692
jstrait <joelstrait@hotmail.com>
2012-03-28 19:37:33 -0400

    Adding CachingWriter tests for stereo files; also making some general code improvements to the existing mono tests as well.

--- test/cachingwriter_test.rb
+++ test/cachingwriter_test.rb
@@ -7,30 +7,57 @@ end
 
 # Basic tests for CachingWriter; the integration tests test it more thoroughly.
 class CachingWriterTest < Test::Unit::TestCase
-  def test_foo
+  def test_mono
+    buffer1_bytes = [0, 0, 1, 0, 2, 0]
+    buffer2_bytes = [3, 0, 4, 0, 5, 0]
+
     format = WaveFile::Format.new(:mono, 16, 44100)
-    writer = StringCachingWriter.new("foo", format)
+    writer = StringCachingWriter.new("does_not_matter", format)
     string_io = StringIO.new
     writer.file = string_io
 
     assert_equal(format, writer.format)
-    assert_equal("", string_io.string)
 
     buffer = WaveFile::Buffer.new([0, 1, 2], format)
     writer.write(buffer)
-    assert_equal("\000\000\001\000\002\000", string_io.string)
-
+    assert_equal(buffer1_bytes, get_bytes(string_io))
+    
     buffer = WaveFile::Buffer.new([3, 4, 5], format)
     writer.write(buffer)
-    assert_equal("\000\000\001\000\002\000" +
-                 "\003\000\004\000\005\000",
-                 string_io.string)
+    assert_equal(buffer1_bytes + buffer2_bytes, get_bytes(string_io))
 
     buffer = WaveFile::Buffer.new([0, 1, 2], format)
     writer.write(buffer)
-    assert_equal("\000\000\001\000\002\000" +
-                   "\003\000\004\000\005\000" +
-                   "\000\000\001\000\002\000",
-                 string_io.string)
+    assert_equal(buffer1_bytes + buffer2_bytes + buffer1_bytes, get_bytes(string_io))
+  end
+
+  def test_stereo
+    buffer1_bytes = [0, 0, 3, 0, 1, 0, 2, 0]
+    buffer2_bytes = [9, 0, 6, 0, 8, 0, 7, 0]
+
+    format = WaveFile::Format.new(:stereo, 16, 44100)
+    writer = StringCachingWriter.new("does_not_matter", format)
+    string_io = StringIO.new
+    writer.file = string_io
+
+    assert_equal(format, writer.format)
+
+    buffer = WaveFile::Buffer.new([[0, 3], [1, 2]], format)
+    writer.write(buffer)
+    assert_equal(buffer1_bytes, get_bytes(string_io))
+
+    buffer = WaveFile::Buffer.new([[9, 6], [8, 7]], format)
+    writer.write(buffer)
+    assert_equal(buffer1_bytes + buffer2_bytes, get_bytes(string_io))
+
+    buffer = WaveFile::Buffer.new([[0, 3], [1, 2]], format)
+    writer.write(buffer)
+    assert_equal(buffer1_bytes + buffer2_bytes + buffer1_bytes, get_bytes(string_io))
+  end
+
+private
+
+  def get_bytes(string_io)
+    string_io.string.each_byte.inject([]) {|arr, element| arr << element }
   end
 end
