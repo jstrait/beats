5b4b1136643042bc559eeeaec61d5039f51ed5a9
jstrait <joel.strait@gmail.com>
2014-02-23 14:45:32 -0800

    Extracting method

--- lib/beats/transforms/song_shuffler.rb
+++ lib/beats/transforms/song_shuffler.rb
@@ -6,20 +6,26 @@ module Beats
           pattern.tracks.values.each do |track|
             original_rhythm = track.rhythm
 
-            track.rhythm = original_rhythm.bytes.each_slice(2).inject("") do |str, slice|
-              str << slice.first
-              if slice.length > 1
-                str << '.' << slice.last
-              end
-
-              str
-            end
+            track.rhythm = swing_16(track.rhythm)
           end
         end
 
         song.tempo = (song.tempo * 1.5).round
 
         song
+      end
+
+      private
+
+      def self.swing_16(original_rhythm)
+        original_rhythm.bytes.each_slice(2).inject("") do |new_rhythm, slice|
+          new_rhythm << slice.first
+          if slice.length > 1
+            new_rhythm << '.' << slice.last
+          end
+
+          new_rhythm
+        end
       end
     end
   end
