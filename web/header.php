9ad234bed6d69736f33ef30df369bed51551b6fc
jstrait <joel.strait@gmail.com>
2015-05-01 22:08:36 -0700

    Updating copyright date

--- web/header.php
+++ web/header.php
@@ -27,7 +27,7 @@ function drawHeader() {
 function drawFooter() {
 ?>
 <div id="about" class="content-box">
-    <p>Copyright &copy; 2010-14 <a href="http://www.joelstrait.com/">Joel Strait</a></p>
+    <p>Copyright &copy; 2010-15 <a href="http://www.joelstrait.com/">Joel Strait</a></p>
   </div>
 </body>
 </html>
