7b77f531e8c01a2c16f4515d8bbd5d113b111f19
jstrait <joel.strait@gmail.com>
2015-05-02 16:42:51 -0700

    Some CSS cleanup

--- web/docs.php
+++ web/docs.php
@@ -2,7 +2,7 @@
    drawHeader(); ?>
   <div class="content-box">
     <h2 id="strategy-guide">Beats Strategy Guide</h2>
-    <p id="hot-tips">Hot tips, straight from the pros!</p>
+    <p class="note">Hot tips, straight from the pros!</p>
     <h3><a href="/tutorial">Getting Started Tutorial</a></h3>
     <p>Learn how to make your first beat.</p>
     <h3><a href="/usage">Usage Instructions</a></h3>
