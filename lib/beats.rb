1ee52c4afa26058f2f71adcf69ff4e1f63776378
Joel Strait <joel.strait@gmail.com>
2018-06-26 09:09:32 -0700

    Renaming source files to use underscores between words

--- lib/beats.rb
+++ lib/beats.rb
@@ -2,17 +2,17 @@ require 'yaml'
 
 gem "wavefile", "=0.8.1"
 require 'wavefile'
-require 'wavefile/cachingwriter'
+require 'wavefile/caching_writer'
 
-require 'beats/audioengine'
-require 'beats/audioutils'
-require 'beats/beatsrunner'
+require 'beats/audio_engine'
+require 'beats/audio_utils'
+require 'beats/beats_runner'
 require 'beats/kit'
 require 'beats/kit_builder'
 require 'beats/pattern'
 require 'beats/song'
-require 'beats/songparser'
-require 'beats/songoptimizer'
+require 'beats/song_parser'
+require 'beats/song_optimizer'
 require 'beats/track'
 require 'beats/transforms/song_swinger'
 
