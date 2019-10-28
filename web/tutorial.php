d8320c1eab17bf34f5dc63eb5bdbb0417f3d6724
Joel Strait <joel.strait@gmail.com>
2019-10-28 11:24:31 -0700

    Various small writing improvements to the tutorial on the website

--- web/tutorial.php
+++ web/tutorial.php
@@ -2,23 +2,23 @@
    drawHeader(); ?>
   <div class="content-box">
     <h2>Getting Started</h2>
-    <p>Let&#8217;s make some beats! First thing, you gotta <a href="/download/">download and install Beats</a>.</p>
+    <p>Let&#8217;s make some beats! First, <a href="/download/">download and install Beats</a>.</p>
     <p>Next, you&#8217;ll need some <code>*.wav</code> sound files. Download this set of <a href="/casio_sa20_drum_sounds.zip">sounds sampled from an ancient Casio keyboard</a>. Unzip that file to a folder.</p>
   </div>
   <div class="content-box">
     <h2>Hello World Beat</h2>
-    <p>Create a new file in the same folder as the drum sounds you just downloaded. Call it something like <code>song.txt</code>. Open it in a text editor and type the following. (Make sure you indent the lines properly).</p>
+    <p>Create a new file in the same folder as the drum sounds you just downloaded. Call it something like <code>song.txt</code>. Open it in a text editor and type the following. (Make sure to indent the lines properly).</p>
     <p><pre><code>Song:
   Flow:
     - Verse
 
 Verse:
   - bass.wav:  X...X...X...X...</code></pre></p>
-    <p>This is about the most minimal song you can create. To turn it into a Wave file, run the following from the command line:</p>
+    <p>This is about the most minimal song you can create. To turn it into a <code>*.wav</code> file, run the following from the command line. (Make sure to run it from the same folder as where the <code>song.txt</code> file is located).</p>
     <p><pre class="single-line"><code>beats song.txt song.wav</code></pre></p>
     <p>If Beats ran successfully, it should print something like the following:</p>
     <p><pre class="single-line"><code>0:02 of audio written in 0.052459 seconds.</code></pre></p>
-    <p>There should now be a file called <code>song.wav</code> in the current directory. Play it in whatever media player you like. It should sound like this:</p>
+    <p>There should now be a file called <code>song.wav</code> in the current folder. Play it in whatever media player you like. It should sound like this:</p>
     <p><audio class="tutorial" src="/media/tutorial_1.wav" controls>Your browser can't play this audio file.</audio></p>
   </div>
   <div class="content-box">
@@ -48,7 +48,7 @@ Verse:
   </div>
   <div class="content-box">
   <h2>Adding More Tracks</h2>
-  <p>That four-on-the-floor bass rhythm is nice, so let&#8217;s build on top of it. To add a snare drum and a hi-hat, just add two more lines to the <code>Verse</code> pattern:</p>
+  <p>That four-on-the-floor bass rhythm is nice, so let&#8217;s build on top of it. To add a snare drum and a hi-hat, add two more lines to the <code>Verse</code> pattern:</p>
   <p><pre><code>Song:
   Tempo: 120
   Flow:
@@ -64,7 +64,7 @@ Verse:
   </div>
   <div class="content-box">
   <h2>Repeating Patterns</h2>
-  <p>You can repeat patterns in the flow. Add <code>x4</code> to repeat the <code>Verse</code> pattern four times.</p>
+  <p>You can repeat patterns in the <code>Flow</code>. Add <code>x4</code> to repeat the <code>Verse</code> pattern four times.</p>
   <p><pre><code>Song:
   Tempo: 120
   Flow:
@@ -80,7 +80,7 @@ Verse:
   </div>
   <div class="content-box">
   <h2>Adding a New Pattern</h2>
-  <p>Now let&#8217;s add another pattern. Call it <code>Chorus</code>. Don&#8217;t forget to add it to the flow. Notice the optional bar line used to separate the two measures in the new pattern.</p>
+  <p>Now let&#8217;s add another pattern. Call it <code>Chorus</code>. Don&#8217;t forget to add it to the <code>Flow</code>. Notice the optional bar line used to separate the two measures in the new pattern.</p>
   <p><pre><code>Song:
   Tempo: 120
   Flow:
@@ -104,8 +104,8 @@ Verse:
   <div class="content-box">
   <h2>Adding a Kit</h2>
   <p>In the last example some of the sounds (<code>bass.wav</code>, <code>snare.wav</code>, <code>hh_closed.wav</code>) were used in two different patterns. If you later wanted to switch out the snare sound with a different sample, you&#8217;d have to update the sound file in both patterns.</p>
-  <p>A <code>kit</code> section lets you adds sounds to your song, and give them a custom label. If you later decide to switch out a sound, you only have to change it in one place. You can also use a different name than the sound file name.</p>
-  <p>Notice how the patterns in this song use both sounds that are defined in the kit, as well as sounds that aren&#8217;t:</p>
+  <p>A <code>Kit</code> section lets you give custom labels to sounds in your song. If you later decide to switch out a sound file, you only have to change it once, in the <code>Kit</code>. This also let&rsquo;s you use a different name than the sound file name.</p>
+  <p>The patterns in the song below use both sounds that are defined in the <code>Kit</code>, as well as sounds that aren&#8217;t:</p>
   <p><pre><code>Song:
   Tempo: 120
 <span class="difference">  Kit:
@@ -132,8 +132,8 @@ Chorus:
   </div>
   <div class="content-box">
   <h2>Swing Beats</h2>
-  <p>These days you&#8217;re nobody if your beats don&#8217;t swing. Fortunately Beats lets you either swing 8th notes (i.e. every two <code>X</code>s or <code>.</code>s) or 16th notes (i.e. every X or <code>.</code>).</p>
-  <p>To do this, just add a <code>Swing</code> to the song header. The value can either be 8 or 16. The song below shows an example.</p>
+  <p>Beats lets you swing 8th notes (i.e. every two <code>X</code>s or <code>.</code>s) or 16th notes (i.e. every X or <code>.</code>).</p>
+  <p>To do this, add a <code>Swing</code> entry to the song header. The value can either be 8 or 16.</p>
   <p><pre><code>Song:
   Tempo: 120
   Kit:
@@ -164,8 +164,8 @@ Chorus:
   </div>
     <div class="content-box">
   <h2>Writing Each Track To a Separate Wave File</h2>
-  <p>If you want to use your beat in music editing software such as Logic, GarageBand, etc., you'll probably want to save each track as a separate Wave file. This way you can independently adjust the volume of each track, add effects to individual sounds, etc.</p>
-  <p>To do this, just add the <code>-s</code> option when running Beats. For example:</p>
+  <p>If you want to use your beat in music software such as Logic, GarageBand, etc., you might want to save each track as a separate <code>*.wav</code> file. This way you can independently mix the volume of each track, add effects to individual sounds, etc.</p>
+  <p>To do this, add the <code>-s</code> option when running Beats. For example:</p>
   <p><pre class="single-line"><code>beats -s song.txt song.wav</code></pre></p>
   <p>If you add this option when running Beats for the following song:</p>
   <p><pre><code>Song:
