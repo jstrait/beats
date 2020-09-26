96f2c620d4031380e2a13f84133a601cc29f2e04
Joel Strait <joel.strait@gmail.com>
2020-09-26 16:13:26 -0700

    More explicit kit item names for tests
    This hopefully makes the kit item names more clear. For example,
    "S_LONGER" seems a little more explanatory than "SL".
    
    The "TE" item, which I believe was short for "T_EXACT", is being
    renamed "T" for consistency because it has the same sample count
    as "S". I don't know what "T" is supposed to mean exactly, other
    than being the next letter after "S". (I think "S" was intended
    to be short for "sound").
    
    For now I'm keeping the short variable names that are used in the
    actual tests, instead of renaming them to match the kit item
    names. The abbreviations seem convenient to make the test
    assertions a little easier to read, and the names seem less
    cryptic now that they can be cross-referenced with the newly
    descriptive kit items names.
    
    The one exception to "no changes to variable names" is that the
    `te` variable is being renamed to `t`, to match the new kit item
    name.

--- test/audio_engine_test.rb
+++ test/audio_engine_test.rb
@@ -46,58 +46,58 @@ class AudioEngineTest < Minitest::Test
   end
 
 
-  # S    Sample data for a sound. Unrealistically short for clarity.
-  # SL   Sound when step sample length is longer than full sound length
-  # SS   Sound when step sample length is less than full sound length
-  # SO   Sound overflow when step sample length is less than full sound length
-  # TE   A step with no sound, with length equal to S
-  # TL   A step with no sound, longer than full sound length
-  # TS   A step with no sound, shorter than full sound length
-  # Z    A zero sample
+  # S           Sample data for a sound. Unrealistically short for clarity.
+  # S_LONG      A slightly longer sound.
+  # S_SHORT     A slightly shorter Sound.
+  # S_OVERFLOW  Sound overflow when step sample length is less than full sound length
+  # T           Sample data for a step with no sound, with length equal to S
+  # T_LONG      Sample data for a step with no sound, longer than full sound length
+  # T_SHORT     Sample data for a step with no sound, shorter than full sound length
+  # ZERO        A single zero sample
 
   MONO_KIT = MockKit.new({}, 1, 16)
-  MONO_KIT.items = { "S"  => [-100, 200, 300, -400],
-                     "SL" => [-100, 200, 300, -400, 0, 0],
-                     "SS" => [-100, 200],
-                     "SO" => [300, -400],
-                     "TE" => [0, 0, 0, 0],
-                     "TL" => [0, 0, 0, 0, 0, 0],
-                     "TS" => [0, 0],
-                     "Z"  => [0] }
+  MONO_KIT.items = { "S" => [-100, 200, 300, -400],
+                     "S_LONG" => [-100, 200, 300, -400, 0, 0],
+                     "S_SHORT" => [-100, 200],
+                     "S_OVERFLOW" => [300, -400],
+                     "T" => [0, 0, 0, 0],
+                     "T_LONG" => [0, 0, 0, 0, 0, 0],
+                     "T_SHORT" => [0, 0],
+                     "ZERO" => [0] }
 
   STEREO_KIT = MockKit.new({}, 2, 16)
-  STEREO_KIT.items = { "S"  => [[-100, 800], [200, -700], [300, -600], [-400, 400]],
-                       "SL" => [[-100, 800], [200, -700], [300, -600], [-400, 400], [0, 0], [0, 0]],
-                       "SS" => [[-100, 800], [200, -700]],
-                       "SO" => [[300, -600], [-400, 400]],
-                       "TE" => [[0, 0], [0, 0], [0, 0], [0, 0]],
-                       "TL" => [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
-                       "TS" => [[0, 0], [0, 0]],
-                       "Z"  => [[0, 0]] }
+  STEREO_KIT.items = { "S" => [[-100, 800], [200, -700], [300, -600], [-400, 400]],
+                       "S_LONG" => [[-100, 800], [200, -700], [300, -600], [-400, 400], [0, 0], [0, 0]],
+                       "S_SHORT" => [[-100, 800], [200, -700]],
+                       "S_OVERFLOW" => [[300, -600], [-400, 400]],
+                       "T" => [[0, 0], [0, 0], [0, 0], [0, 0]],
+                       "T_LONG" => [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+                       "T_SHORT" => [[0, 0], [0, 0]],
+                       "ZERO" => [[0, 0]] }
 
 
   # These tests use unrealistically short sounds and step sample lengths, to make tests easier to work with.
   def test_generate_track_sample_data
     [MONO_KIT, STEREO_KIT].each do |kit|
       s  = kit.get_sample_data("S")
-      sl = kit.get_sample_data("SL")
-      ss = kit.get_sample_data("SS")
-      so = kit.get_sample_data("SO")
-      te = kit.get_sample_data("TE")
-      tl = kit.get_sample_data("TL")
-      ts = kit.get_sample_data("TS")
-      z  = kit.get_sample_data("Z")
+      sl = kit.get_sample_data("S_LONG")
+      ss = kit.get_sample_data("S_SHORT")
+      so = kit.get_sample_data("S_OVERFLOW")
+      t  = kit.get_sample_data("T")
+      tl = kit.get_sample_data("T_LONG")
+      ts = kit.get_sample_data("T_SHORT")
+      z  = kit.get_sample_data("ZERO")
 
       # 1.) Tick sample length is equal to the length of the sound sample data.
       #     When this is the case, overflow should never occur.
       #     In practice, this will probably not occur often, but these tests act as a form of sanity check.
       helper_generate_track_sample_data(kit, "",       4, [])
       helper_generate_track_sample_data(kit, "X",      4, s)
-      helper_generate_track_sample_data(kit, "X.",     4, s + te)
-      helper_generate_track_sample_data(kit, ".X",     4, te + s)
-      helper_generate_track_sample_data(kit, "...X.",  4, (te * 3) + s + te)
-      helper_generate_track_sample_data(kit, ".X.XX.", 4, te + s + te + s + s + te)
-      helper_generate_track_sample_data(kit, "...",    4, te * 3)
+      helper_generate_track_sample_data(kit, "X.",     4, s + t)
+      helper_generate_track_sample_data(kit, ".X",     4, t + s)
+      helper_generate_track_sample_data(kit, "...X.",  4, (t * 3) + s + t)
+      helper_generate_track_sample_data(kit, ".X.XX.", 4, t + s + t + s + s + t)
+      helper_generate_track_sample_data(kit, "...",    4, t * 3)
 
       # 2A.) Tick sample length is longer than the sound sample data. This is similar to (1), except that there should
       #     be some extra silence after the end of each trigger.
@@ -108,7 +108,7 @@ class AudioEngineTest < Minitest::Test
       helper_generate_track_sample_data(kit, ".X",     6, tl + sl)
       helper_generate_track_sample_data(kit, "...X.",  6, (tl * 3) + sl + tl)
       helper_generate_track_sample_data(kit, ".X.XX.", 6, tl + sl + tl + sl + sl + tl)
-      helper_generate_track_sample_data(kit, "...",    6, (te + ts) * 3)
+      helper_generate_track_sample_data(kit, "...",    6, (t + ts) * 3)
 
       # 2B.) Tick sample length is longer than the sound sample data, but not by an integer amount.
       #
@@ -168,16 +168,16 @@ class AudioEngineTest < Minitest::Test
 
   def test_composite_pattern_tracks
     no_overflow_tracks = [
-      Track.new("S",  "X..."),
-      Track.new("SO", "X.X."),
-      Track.new("S",  "X.XX"),
+      Track.new("S", "X..."),
+      Track.new("S_OVERFLOW", "X.X."),
+      Track.new("S", "X.XX"),
     ]
     no_overflow_pattern = Pattern.new("no_overflow", no_overflow_tracks)
 
     overflow_tracks = [
-      Track.new("S",  "X..X"),
-      Track.new("SO", "XX.X"),
-      Track.new("SL", ".X.X"),
+      Track.new("S", "X..X"),
+      Track.new("S_OVERFLOW", "XX.X"),
+      Track.new("S_LONG", ".X.X"),
     ]
     overflow_pattern = Pattern.new("overflow", overflow_tracks)
 
@@ -193,7 +193,7 @@ class AudioEngineTest < Minitest::Test
                     0 + 0 + -100,        0 + 0 + 200,        0 + 0 + 300,     0 + 0 + -400,
                  ],
                  primary)
-    assert_equal({"S" => [], "SO" => [], "S2" => []}, overflow)
+    assert_equal({"S" => [], "S_OVERFLOW" => [], "S2" => []}, overflow)
 
 
     # Simple case, no overflow (stereo)
@@ -219,7 +219,7 @@ class AudioEngineTest < Minitest::Test
                         [0 + 0 + -400,   0 + 0 + 400],
                  ],
                  primary)
-    assert_equal({"S" => [], "SO" => [], "S2" => []}, overflow)
+    assert_equal({"S" => [], "S_OVERFLOW" => [], "S2" => []}, overflow)
 
 
     # Some overflow (mono)
@@ -233,7 +233,7 @@ class AudioEngineTest < Minitest::Test
                     -100 + 300 + -100,   200 + -400 + 200,   300 + 0 + 300,
                  ],
                  primary)
-    assert_equal({"S" => [-400], "SO" => [], "SL" => [-400, 0, 0]}, overflow)
+    assert_equal({"S" => [-400], "S_OVERFLOW" => [], "S_LONG" => [-400, 0, 0]}, overflow)
 
 
     # Some overflow (stereo)
@@ -255,6 +255,6 @@ class AudioEngineTest < Minitest::Test
                         [300 + 0 + 300,       -600 + 0 + -600],
                  ],
                  primary)
-    assert_equal({"S" => [[-400, 400]], "SO" => [], "SL" => [[-400, 400], [0, 0], [0, 0]]}, overflow)
+    assert_equal({"S" => [[-400, 400]], "S_OVERFLOW" => [], "S_LONG" => [[-400, 400], [0, 0], [0, 0]]}, overflow)
   end
 end
