41ca1f98b8d49aa1c5e576cb620e848a564b64b2
Joel Strait <joel.strait@gmail.com>
2020-10-06 10:10:51 -0700

    Refactoring `Kit` instances used for `AudioEngine` tests
    Previously, the `Kit` instances set up for these tests contained
    items both for sounds used in tracks, as well as sounds that
    represented expected results of audio generation output. This
    mixing of concepts confused me when reading this code for the
    first time in a long time, until I figured out what was going on.
    
    The fixture `Kit` instances now only contain items for sounds that
    are directly used in tracks. Objects that represent expected audio
    generation output are now manually defined as needed by tests. The
    hopeful result is that these tests are now easier to understand.

--- test/audio_engine_test.rb
+++ test/audio_engine_test.rb
@@ -12,69 +12,52 @@ class AudioEngineTest < Minitest::Test
   end
 
 
-  # S           Sample data for a sound. Unrealistically short for clarity.
-  # S_LONG      A slightly longer sound.
-  # S_SHORT     A slightly shorter Sound.
-  # S_OVERFLOW  Sound overflow when step sample length is less than full sound length
-  # T           Sample data for a step with no sound, with length equal to S
-  # T_LONG      Sample data for a step with no sound, longer than full sound length
-  # T_SHORT     Sample data for a step with no sound, shorter than full sound length
-  # ZERO        A single zero sample
-
-  MONO_KIT_ITEMS = { "S" => [-100, 200, 300, -400],
-                     "S_LONG" => [-100, 200, 300, -400, 0, 0],
-                     "S_SHORT" => [-100, 200],
-                     "S_OVERFLOW" => [300, -400],
-                     "T" => [0, 0, 0, 0],
-                     "T_LONG" => [0, 0, 0, 0, 0, 0],
-                     "T_SHORT" => [0, 0],
-                     "ZERO" => [0] }
+  MONO_KIT_ITEMS = { "sound" => [-100, 200, 300, -400],
+                     "longer_sound" => [-100, 200, 300, -400, 0, 0],
+                     "shorter_sound" => [300, -400],
+                     "zero_sample" => [0] }
   MONO_KIT = Kit.new(MONO_KIT_ITEMS, 1, 16)
 
-  STEREO_KIT_ITEMS = { "S" => [[-100, 800], [200, -700], [300, -600], [-400, 400]],
-                       "S_LONG" => [[-100, 800], [200, -700], [300, -600], [-400, 400], [0, 0], [0, 0]],
-                       "S_SHORT" => [[-100, 800], [200, -700]],
-                       "S_OVERFLOW" => [[300, -600], [-400, 400]],
-                       "T" => [[0, 0], [0, 0], [0, 0], [0, 0]],
-                       "T_LONG" => [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
-                       "T_SHORT" => [[0, 0], [0, 0]],
-                       "ZERO" => [[0, 0]] }
+  STEREO_KIT_ITEMS = { "sound" => [[-100, 800], [200, -700], [300, -600], [-400, 400]],
+                       "longer_sound" => [[-100, 800], [200, -700], [300, -600], [-400, 400], [0, 0], [0, 0]],
+                       "shorter_sound" => [[300, -600], [-400, 400]],
+                       "zero_sample" => [[0, 0]] }
   STEREO_KIT = Kit.new(STEREO_KIT_ITEMS, 2, 16)
 
 
   # These tests use unrealistically short sounds and step sample lengths, to make tests easier to work with.
+  #
+  # "b" is short for "beat", and "r" is short for "rest", and "o" is short for "overflow"
   def test_generate_track_sample_data
     [MONO_KIT, STEREO_KIT].each do |kit|
-      s  = kit.get_sample_data("S")
-      sl = kit.get_sample_data("S_LONG")
-      ss = kit.get_sample_data("S_SHORT")
-      so = kit.get_sample_data("S_OVERFLOW")
-      t  = kit.get_sample_data("T")
-      tl = kit.get_sample_data("T_LONG")
-      ts = kit.get_sample_data("T_SHORT")
-      z  = kit.get_sample_data("ZERO")
+      sound_samples = kit.get_sample_data("sound")
+      zero_sample = kit.get_sample_data("zero_sample")
 
       # 1.) Tick sample length is equal to the length of the sound sample data.
       #     When this is the case, overflow should never occur.
       #     In practice, this will probably not occur often, but these tests act as a form of sanity check.
+      b = sound_samples
+      r = zero_sample * 4
       helper_generate_track_sample_data(kit, "",       4, [])
-      helper_generate_track_sample_data(kit, "X",      4, s)
-      helper_generate_track_sample_data(kit, "X.",     4, s + t)
-      helper_generate_track_sample_data(kit, ".X",     4, t + s)
-      helper_generate_track_sample_data(kit, "...X.",  4, (t * 3) + s + t)
-      helper_generate_track_sample_data(kit, ".X.XX.", 4, t + s + t + s + s + t)
-      helper_generate_track_sample_data(kit, "...",    4, t * 3)
+      helper_generate_track_sample_data(kit, "X",      4, b)
+      helper_generate_track_sample_data(kit, "X.",     4, b + r)
+      helper_generate_track_sample_data(kit, ".X",     4, r + b)
+      helper_generate_track_sample_data(kit, "...X.",  4, r + r + r + b + r)
+      helper_generate_track_sample_data(kit, ".X.XX.", 4, r + b + r + b + b + r)
+      helper_generate_track_sample_data(kit, "...",    4, r + r + r)
 
       # 2A.) Tick sample length is longer than the sound sample data. This is similar to (1), except that there should
       #     be some extra silence after the end of each trigger.
       #     Like (1), overflow should never occur.
+      b = sound_samples + zero_sample + zero_sample
+      r = zero_sample * 6
       helper_generate_track_sample_data(kit, "",       6, [])
-      helper_generate_track_sample_data(kit, "X",      6, sl)
-      helper_generate_track_sample_data(kit, "X.",     6, sl + tl)
-      helper_generate_track_sample_data(kit, ".X",     6, tl + sl)
-      helper_generate_track_sample_data(kit, "...X.",  6, (tl * 3) + sl + tl)
-      helper_generate_track_sample_data(kit, ".X.XX.", 6, tl + sl + tl + sl + sl + tl)
-      helper_generate_track_sample_data(kit, "...",    6, (t + ts) * 3)
+      helper_generate_track_sample_data(kit, "X",      6, b)
+      helper_generate_track_sample_data(kit, "X.",     6, b + r)
+      helper_generate_track_sample_data(kit, ".X",     6, r + b)
+      helper_generate_track_sample_data(kit, "...X.",  6, r + r + r + b + r)
+      helper_generate_track_sample_data(kit, ".X.XX.", 6, r + b + r + b + b + r)
+      helper_generate_track_sample_data(kit, "...",    6, r + r + r)
 
       # 2B.) Tick sample length is longer than the sound sample data, but not by an integer amount.
       #
@@ -82,27 +65,36 @@ class AudioEngineTest < Minitest::Test
       # Tick:               1,     2,     3,     4,     5,     6
       # Raw:        0.0, 5.83, 11.66, 17.49, 23.32, 29.15, 34.98
       # Quantized:    0,    5,    11,    17,    23,    29,    34
+      b5 = sound_samples + zero_sample                 # Beat that is 5 samples long
+      b6 = sound_samples + zero_sample + zero_sample   # Beat that is 6 samples long
+      r5 = zero_sample * 5                             # Rest that is 5 samples long
+      r6 = zero_sample * 6                             # Rest that is 6 samples long
       helper_generate_track_sample_data(kit, "",       5.83, [])
-      helper_generate_track_sample_data(kit, "X",      5.83, sl[0..4])
-      helper_generate_track_sample_data(kit, "X.",     5.83, sl[0..4] + tl)
-      helper_generate_track_sample_data(kit, ".X",     5.83, tl[0..4] + sl)
-      helper_generate_track_sample_data(kit, "...X.",  5.83, (z * 17) + sl + tl)
-      helper_generate_track_sample_data(kit, ".X.XX.", 5.83, tl[0..4] + sl + tl + sl + sl + tl[0..4])
-      helper_generate_track_sample_data(kit, "...",    5.83, z * 17)
+      helper_generate_track_sample_data(kit, "X",      5.83, b5)
+      helper_generate_track_sample_data(kit, "X.",     5.83, b5 + r6)
+      helper_generate_track_sample_data(kit, ".X",     5.83, r5 + b6)
+      helper_generate_track_sample_data(kit, "...X.",  5.83, r5 + r6 + r6 + b6 + r6)
+      helper_generate_track_sample_data(kit, ".X.XX.", 5.83, r5 + b6 + r6 + b6 + b6 + r5)
+      helper_generate_track_sample_data(kit, "...",    5.83, r5 + r6 + r6)
 
       # 3A.) Tick sample length is shorter than the sound sample data. Overflow will now occur!
-      helper_generate_track_sample_data(kit, "",       2, [],              [])
-      helper_generate_track_sample_data(kit, "X",      2, ss,              so)
-      helper_generate_track_sample_data(kit, "X.",     2, s,               [])
-      helper_generate_track_sample_data(kit, ".X",     2, ts + ss,         so)
-      helper_generate_track_sample_data(kit, "...X.",  2, (ts * 3) + s,    [])
-      helper_generate_track_sample_data(kit, ".X.XX.", 2, ts + s + ss + s, [])
-      helper_generate_track_sample_data(kit, "...",    2, z * 6,           [])
+      b = sound_samples[0..1]
+      o = sound_samples[2..3]
+      r = zero_sample * 2
+      helper_generate_track_sample_data(kit, "",       2, [],                    [])
+      helper_generate_track_sample_data(kit, "X",      2, b,                     o)
+      helper_generate_track_sample_data(kit, "X.",     2, b + o,                 [])
+      helper_generate_track_sample_data(kit, ".X",     2, r + b,                 o)
+      helper_generate_track_sample_data(kit, "...X.",  2, r + r + r + b + o,     [])
+      helper_generate_track_sample_data(kit, ".X.XX.", 2, r + b + o + b + b + o, [])
+      helper_generate_track_sample_data(kit, "...",    2, r + r + r,             [])
 
       # 3B.) Tick sample length is shorter than sound sample data, such that a beat other than the final one
       #      would extend past the end of the rhythm if not cut off. Make sure that the sample data array doesn't
       #      inadvertently lengthen as a result.
-      helper_generate_track_sample_data(kit, "XX", 1, [s[0], s[0]], s[1..3])
+      b = sound_samples[0..0]
+      o = sound_samples[1..3]
+      helper_generate_track_sample_data(kit, "XX", 1, b + b, o)
 
       # 3C.) Tick sample length is shorter than the sound sample data, but not by an integer amount.
       #
@@ -110,13 +102,16 @@ class AudioEngineTest < Minitest::Test
       # Tick:               1,    2,    3,    4,    5,     6
       # Raw:        0.0, 1.83, 3.66, 5.49, 7.32, 9.15, 10.98
       # Quantized:    0,    1,    3,    5,    7,    9,    10
-      helper_generate_track_sample_data(kit, "",       1.83,                       [])
-      helper_generate_track_sample_data(kit, "X",      1.83, s[0..0],              s[1..3])
-      helper_generate_track_sample_data(kit, "X.",     1.83, s[0..2],              s[3..3])
-      helper_generate_track_sample_data(kit, ".X",     1.83, z + s[0..1],          s[2..3])
-      helper_generate_track_sample_data(kit, "...X.",  1.83, (z * 5) + s,          [])
-      helper_generate_track_sample_data(kit, ".X.XX.", 1.83, z + s + ss + s[0..2], s[3..3])
-      helper_generate_track_sample_data(kit, "...",    1.83, z * 5,                [])
+      b = sound_samples
+      r1 = zero_sample      # Rest that is 1 sample long
+      r2 = zero_sample * 2  # Rest that is 2 samples long
+      helper_generate_track_sample_data(kit, "",       1.83,                                             [])
+      helper_generate_track_sample_data(kit, "X",      1.83, b[0..0],                                    b[1..3])
+      helper_generate_track_sample_data(kit, "X.",     1.83, b[0..0] + b[1..2],                          b[3..3])
+      helper_generate_track_sample_data(kit, ".X",     1.83, r1 + b[0..1],                               b[2..3])
+      helper_generate_track_sample_data(kit, "...X.",  1.83, r1 + r2 + r2 + b[0..1] + b[2..3],           [])
+      helper_generate_track_sample_data(kit, ".X.XX.", 1.83, r1 + b[0..1] + b[2..3] + b[0..1] + b[0..2], b[3..3])
+      helper_generate_track_sample_data(kit, "...",    1.83, r1 + r2 + r2,                               [])
     end
   end
 
@@ -126,7 +121,7 @@ class AudioEngineTest < Minitest::Test
 
     audio_engine = AudioEngine.new(song, kit)
     assert_equal(step_sample_length, audio_engine.step_sample_length)
-    actual = audio_engine.send(:generate_track_sample_data, track, kit.get_sample_data("S"))
+    actual = audio_engine.send(:generate_track_sample_data, track, kit.get_sample_data("sound"))
 
     assert_equal(Hash,                     actual.class)
     assert_equal(["overflow", "primary"],  actual.keys.map{|key| key.to_s}.sort)
@@ -136,16 +131,16 @@ class AudioEngineTest < Minitest::Test
 
   def test_composite_pattern_tracks
     no_overflow_tracks = [
-      Track.new("S", "X..."),
-      Track.new("S_OVERFLOW", "X.X."),
-      Track.new("S", "X.XX"),
+      Track.new("sound", "X..."),
+      Track.new("shorter_sound", "X.X."),
+      Track.new("sound", "X.XX"),
     ]
     no_overflow_pattern = Pattern.new("no_overflow", no_overflow_tracks)
 
     overflow_tracks = [
-      Track.new("S", "X..X"),
-      Track.new("S_OVERFLOW", "XX.X"),
-      Track.new("S_LONG", ".X.X"),
+      Track.new("sound", "X..X"),
+      Track.new("shorter_sound", "XX.X"),
+      Track.new("longer_sound", ".X.X"),
     ]
     overflow_pattern = Pattern.new("overflow", overflow_tracks)
 
@@ -161,7 +156,7 @@ class AudioEngineTest < Minitest::Test
                     0 + 0 + -100,        0 + 0 + 200,        0 + 0 + 300,     0 + 0 + -400,
                  ],
                  primary)
-    assert_equal({"S" => [], "S_OVERFLOW" => [], "S2" => []}, overflow)
+    assert_equal({"sound" => [], "shorter_sound" => [], "sound2" => []}, overflow)
 
 
     # Simple case, no overflow (stereo)
@@ -187,7 +182,7 @@ class AudioEngineTest < Minitest::Test
                         [0 + 0 + -400,   0 + 0 + 400],
                  ],
                  primary)
-    assert_equal({"S" => [], "S_OVERFLOW" => [], "S2" => []}, overflow)
+    assert_equal({"sound" => [], "shorter_sound" => [], "sound2" => []}, overflow)
 
 
     # Some overflow (mono)
@@ -201,7 +196,7 @@ class AudioEngineTest < Minitest::Test
                     -100 + 300 + -100,   200 + -400 + 200,   300 + 0 + 300,
                  ],
                  primary)
-    assert_equal({"S" => [-400], "S_OVERFLOW" => [], "S_LONG" => [-400, 0, 0]}, overflow)
+    assert_equal({"sound" => [-400], "shorter_sound" => [], "longer_sound" => [-400, 0, 0]}, overflow)
 
 
     # Some overflow (stereo)
@@ -223,7 +218,7 @@ class AudioEngineTest < Minitest::Test
                         [300 + 0 + 300,       -600 + 0 + -600],
                  ],
                  primary)
-    assert_equal({"S" => [[-400, 400]], "S_OVERFLOW" => [], "S_LONG" => [[-400, 400], [0, 0], [0, 0]]}, overflow)
+    assert_equal({"sound" => [[-400, 400]], "shorter_sound" => [], "longer_sound" => [[-400, 400], [0, 0], [0, 0]]}, overflow)
   end
 
   # Creates a song with a tempo such that each step will have the
